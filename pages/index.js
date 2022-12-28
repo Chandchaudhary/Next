bf78ad7374dcf376a65b3984aea73d80186e8812
Chandjewellerskart <115988966+Chandjewellerskart@users.noreply.github.com>
2022-12-28 12:05:03 +0530

    2.0

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold ">JewellersKart</h1>
+      <h1 className="text-3xl font-bold ">JewellersKart 2.0</h1>
     </>
   );
 }
